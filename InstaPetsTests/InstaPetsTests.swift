51564e6b717e797949aa164f56650672c708de64
Wilmer <wilmersinchi@gmail.com>
2019-10-31 12:17:19 -0400

    Initial Commit

---
+++ InstaPetsTests/InstaPetsTests.swift
@@ -0,0 +1,34 @@
+//
+//  InstaPetsTests.swift
+//  InstaPetsTests
+//
+//  Created by Wilmer sinchi on 10/31/19.
+//  Copyright © 2019 Wilmer sinchi. All rights reserved.
+//
+
+import XCTest
+@testable import InstaPets
+
+class InstaPetsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
